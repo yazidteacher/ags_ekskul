aa7ef532cb13f74df995cd662592355464818250
yazidteacher <ayazid.teacher@altaglobalsch.com>
2026-04-15 12:29:49 +0000

    creating first streamlit app

---
+++ streamlit-apps/first-app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+#to write the title
+st.title("My First Web App")
+
+st.header("MY Favorite food")
+st.subheader("1. Goat Satay")
+
+#to write a text
+st.write("The websites is sooo amazing. I could write anything I wanted.")
+
+st.write("this is the goat satay that i like")
+
+if st.button("See me"):
+    st.write("☠️🍳🍳🌟🍳🌟🍳🌟😭🌟 APril Fools")
+    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv9tetncPTfXLy25u0Dr40rgkUqw0irZHL7A&s")
+    st.balloons()
+
+
+st.caption("This is a small text")
